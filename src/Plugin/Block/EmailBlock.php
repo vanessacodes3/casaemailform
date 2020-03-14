4c9f82822e38742a2fa0e513448cd4d26ac03d19
vecarroll3 <62161384+vecarroll3@users.noreply.github.com>
2020-03-13 18:40:28 -0700

    Updated EmailBlock.php
    Added form to block.

--- src/Plugin/Block/EmailBlock.php
+++ src/Plugin/Block/EmailBlock.php
@@ -3,6 +3,9 @@
 namespace Drupal\casaemailform\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Form\FormBuilderInterface;
 
 /**
  * Provides a 'Email Form' Block.
@@ -16,12 +19,45 @@ use Drupal\Core\Block\BlockBase;
 class EmailBlock extends BlockBase {
 
   /**
+   * @var $formbuilder \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected $formbuilder;
+
+  /**
+   * Constructs a new Email Form Block.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Form\FormBuilderInterface $form_builder
+   *   The form builder.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilderInterface $form_builder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $form_builder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder')
+    );
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function build() {
-    return [
-      '#markup' => $this->t('Hello, World!'),
-    ];
+  $build['form'] = $this->formBuilder()->getForm('Drupal\casaemailform\Form\CASAForm'); 
+  return $build;
   }
 
 }
